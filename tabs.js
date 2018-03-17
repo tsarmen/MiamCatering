08c3a4abf66a1b947d688f878f312af906999af4
Silver Vapper <silvervapper@live.com>
2018-03-17 16:41:21 +0100

    tabs.js toetab k6iki tyype, ebavajalikud htmlid eemaldatud

--- tabs.js
+++ tabs.js
@@ -1,8 +1,12 @@
 document.getElementsByClassName("tabcontent firma")[0].click();
 document.getElementsByClassName("tablinks firma")[0].click();
+document.getElementsByClassName("tabcontent pulm")[0].click();
+document.getElementsByClassName("tablinks pulm")[0].click();
 document.getElementsByClassName("tabcontent synna")[0].click();
 document.getElementsByClassName("tablinks synna")[0].click();
-arrayOfActiveTabs = [0,0];
+document.getElementsByClassName("tabcontent kodukokk")[0].click();
+document.getElementsByClassName("tablinks kodukokk")[0].click();
+arrayOfActiveTabs = [0,0,0,0];
 
 function uusFirma(evt, retseptNr, tyyp) {
     var i, tabcontent, tablinks;
@@ -18,21 +22,13 @@ function uusFirma(evt, retseptNr, tyyp) {
     tabcontent[activeTab].style.display = "block";
     evt.currentTarget.className += " active";
 
-    if(tyyp == "firma"){
-      arrayOfActiveTabs[0]=activeTab;
-    }else if (tyyp == "synna") {
-      arrayOfActiveTabs[1]=activeTab;
-    }
+    setActiveTab(tyyp,activeTab)
 }
 
 function nextFirma(evt, tyyp){
     var i, tabcontent, tablinks;
 
-    if(tyyp == "firma"){
-      activeTab = arrayOfActiveTabs[0];
-    }else if (tyyp == "synna") {
-      activeTab = arrayOfActiveTabs[1];
-    }
+    activeTab = getCorrectActiveTab(tyyp);
 
     tablinks = document.getElementsByClassName("tablinks " + tyyp);
     if(activeTab + 1 < tablinks.length){
@@ -55,21 +51,13 @@ function nextFirma(evt, tyyp){
     }
     tabcontent[activeTab].style.display = "block";
 
-    if(tyyp == "firma"){
-      arrayOfActiveTabs[0]=activeTab;
-    }else if (tyyp == "synna") {
-      arrayOfActiveTabs[1]=activeTab;
-    }
+    setActiveTab(tyyp, activeTab)
 }
 
 function prevFirma(evt, tyyp){
     var i, tabcontent, tablinks;
 
-    if(tyyp == "firma"){
-      activeTab = arrayOfActiveTabs[0];
-    }else if (tyyp == "synna") {
-      activeTab = arrayOfActiveTabs[1];
-    }
+    activeTab = getCorrectActiveTab(tyyp);
 
     tablinks = document.getElementsByClassName("tablinks " + tyyp);
     if(activeTab - 1 >= 0){
@@ -92,10 +80,29 @@ function prevFirma(evt, tyyp){
     }
     tabcontent[activeTab].style.display = "block";
 
+    setActiveTab(tyyp, activeTab)
+}
 
-    if(tyyp == "firma"){
-      arrayOfActiveTabs[0]=activeTab;
-    }else if (tyyp == "synna") {
-      arrayOfActiveTabs[1]=activeTab;
-    }
+function getCorrectActiveTab(tyyp){
+  if(tyyp == "firma"){
+    return arrayOfActiveTabs[0];
+  }else if (tyyp == "pulm") {
+    return arrayOfActiveTabs[1];
+  }else if (tyyp == "synna") {
+    return arrayOfActiveTabs[2];
+  }else if (tyyp == "kodukokk") {
+    return arrayOfActiveTabs[3];
+  }
+}
+
+function setActiveTab(tyyp, activeTab){
+  if(tyyp == "firma"){
+    arrayOfActiveTabs[0]=activeTab;
+  }else if (tyyp == "pulm") {
+    arrayOfActiveTabs[2]=activeTab;
+  }else if (tyyp == "synna") {
+    arrayOfActiveTabs[2]=activeTab;
+  }else if(tyyp == "kodukokk"){
+    arrayOfActiveTabs[3]=activeTab;
+  }
 }
